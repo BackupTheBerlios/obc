2377ba3656777a9b32873eebb9d70358ffca07b0
dracks <dracks>
2008-03-12 22:17:29 +0000

    Fitxeros para provar la OCCadena

---
+++ objetivoc/pruebas/PruebaOCCadena.h
@@ -0,0 +1,23 @@
+//
+//  PruebaOCCadena.h
+//  Objetivo C
+//
+//  Created by Dracks on 12/03/08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface PruebaOCCadena : Object {
+(OCCadena*) una;
+}
+
+-(OCCadena*) init;
+-(void)	Longitud;
+-(void) ComparaCadenaIgual;
+-(void) ComparaCadenaDistinta;
+-(void) Cadena;
+-(void) subcadena;
+
+@end
---
+++ objetivoc/pruebas/PruebaOCCadena.m
@@ -0,0 +1,14 @@
+//
+//  PruebaOCCadena.m
+//  Objetivo C
+//
+//  Created by Dracks on 12/03/08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import "PruebaOCCadena.h"
+
+
+@implementation PruebaOCCadena
+
+@end
