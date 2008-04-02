3ff9698d5c480442442590a1ddfc0cbdcc275833
dracks <dracks>
2008-04-02 21:43:11 +0000

    implementada OCTokenizer

--- objetivoc/utiles/OCTokenizer.m
+++ objetivoc/utiles/OCTokenizer.m
@@ -7,8 +7,52 @@
 //
 
 #import "OCTokenizer.h"
+#import <string.h>
+//#import
 
 
 @implementation OCTokenizer
+-(OCTokenizer*)	iniciaConCaracteres: (char*) cadena rotosPor: (char) c{
+	int			i,old,tam;
+	char*		tmp;
+	[super init];
+	
+	tam=strlen(cadena)+1;
+	tmp = malloc(tam*sizeof(char));
+	old=0;
+	
+	lista=[[OCLista alloc] init];
+	for (i=0;i<tam;i++){
+		if (cadena[i]==c){
+			strncpy(tmp,&(cadena[old]), (i-old));
+			old=i;
+			[lista ponerEnCola: (id) [[OCCadena alloc] iniciaConCaracteres: tmp]];
+		}
+	}
+	strncpy(tmp,&(cadena[old]), (tam-old));
+	[lista ponerEnCola: (id) [[OCCadena alloc] iniciaConCaracteres: tmp]];
+	
+	
+	free(tmp);
+	
+	actual=[lista inicio];
+	return self;
+}
+-(OCTokenizer*) reinicia{
+	actual=[lista inicio];
+	return self;
+}
+-(OCCadena*)	actual{
+	return (OCCadena*) [actual dato];
+}
+-(OCCadena*)	siguiente{
+	actual=[actual siguiente];
+	return (OCCadena*) [actual dato];
+}
+-(void)			free{
+	[lista free];
+	[super free];
+}
+
 
 @end
